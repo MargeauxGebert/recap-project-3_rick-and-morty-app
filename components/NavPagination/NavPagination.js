3bd3dbff1ebf6affcc568770f17ea8fe77e7286c
Nikolas <nik.steppenwolf@web.de>
2025-02-26 14:50:21 +0100

    first steps pagination

--- components/NavPagination/NavPagination.js
+++ components/NavPagination/NavPagination.js
@@ -1,7 +1,8 @@
 export function setPaginationMax(apiData, paginationElement) {
+  console.log(apiData.info);
   const maxPages = apiData.info.pages;
   const nextPageCall = apiData.info.next;
   const currentPage = nextPageCall.split("=").pop() - 1;
 
-  paginationElement.textValue = `${currentPage} / ${maxPages}`;
+  paginationElement.textContent = `${currentPage} / ${maxPages}`;
 }
