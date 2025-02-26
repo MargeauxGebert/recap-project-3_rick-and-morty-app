015c5d07048c56275c1ed2447e25b88dfcf2452d
Lisa Hauschild <lisa_hauschild@hotmail.com>
2025-02-26 12:59:05 +0100

    create conflicts

--- components/NavButton/NavButton.js
+++ components/NavButton/NavButton.js
@@ -4,3 +4,25 @@
 const nextButton = document. addEventListener("[`data-js=button--next`]");
 
 const prevButton = document. addEventListener("[`data-js=button-prev`]");
+
+
+if ( === 42) {
+          
+    nextButton.classList.add("disabled");
+  
+  }
+   else if (pageNumber===1){ 
+  
+    prevButton.classList.add("disabled");
+  
+   }
+  
+   else {
+   
+    page++;
+    return //</pageIndex>;    cannot return and then ++
+    // where to add to? 
+    
+  
+  }
+  
--- components/NavPagination/NavPagination.js
+++ components/NavPagination/NavPagination.js
@@ -3,24 +3,4 @@
 
     
   
-        if ( === 42) {
-          
-          nextButton.classList.add("disabled");
-  
-        }
-         else if (pageNumber===1){ 
-  
-          prevButton.classList.add("disabled");
-  
-         }
-  
-         else {
-         
-          page++;
-          return //</pageIndex>;    cannot return and then ++
-          // where to add to? 
-          
-  
-        }
-        }
-  
+     
